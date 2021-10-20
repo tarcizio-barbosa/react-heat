73303ee16c8af2c2d7d21ea17326ae525598c123
Tarcizio B <tarcizio.barbosa@outlook.com>
2021-10-19 23:50:35 -0300

    create a login and list messages components and connect with the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
+import styles from './styles/App.module.scss';
+
 export function App() {
   return (
-    <div>Hello Vite</div>
-  )
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
+  );
 }
